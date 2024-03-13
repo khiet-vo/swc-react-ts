5f87374cb1c58f53abf93b142e6b283fbd280184
KhietVo <khietvo1988@gmail.com>
2024-03-13 17:30:27 +0700

    update script setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-
+# Install nvm if not installed
+if ! command -v nvm &> /dev/null; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+# Check if current node version is not installed
+if ! nvm use &> /dev/null; then
+  # Install new node version
+  nvm install
+fi
 # Install bun
-npm install -g bun
+if ! command -v bun &> /dev/null; then
+  npm install -g bun
+fi
 
-# Run bun install
 bun install
